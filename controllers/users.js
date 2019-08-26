8e4031ae1ec620ce0ea9662f18ecc0b92f8de8f7
gps2601 <smith260194@gmail.com>
2019-08-26 22:01:37 +0100

    Adding endpoint to get all users

--- controllers/users.js
+++ controllers/users.js
@@ -22,4 +22,9 @@ usersRouter.post('/', async (request, response, next) => {
   }
 })
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(u => u.toJSON()))
+})
+
 module.exports = usersRouter
