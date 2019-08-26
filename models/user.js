5e1d419d540c2ff1732912b2f182cf2c7e199719
gps2601 <smith260194@gmail.com>
2019-08-26 22:00:18 +0100

    Adding validation on unique usernames

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const userSchema = mongoose.SchemaType({
-  username: String,
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -12,6 +16,8 @@ const userSchema = mongoose.SchemaType({
   ],
 })
 
+userSchema.plugin(uniqueValidator)
+
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -21,6 +27,4 @@ userSchema.set('toJSON', {
   }
 })
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)
